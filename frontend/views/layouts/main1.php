174f8a017654e42985eea39fd341af84a6a0a818
nurik1kg <nurik1kg@gmail.com>
2016-12-05 08:08:22 +0300

    layaout/amin.php

--- frontend/views/layouts/main1.php
+++ frontend/views/layouts/main1.php
@@ -34,10 +34,10 @@ ChangeLanguageAsset::register($this);
 <div class="top">
     <div class="uk-grid uk-grid-collapse">
         <div class="uk-width-1-3">
-            <div class="call"><a href="tel:<?=' '.Yii::t('app','source_number')?>" style="color: #fff;"><span class="uk-icon-phone-square"></span> <?=' '.Yii::t('app','source_number')?></a></div>
+            <div class="call"><span class="uk-icon-phone-square"><?=' '.Yii::t('app','source_number')?></div>
         </div>
         <div class="uk-width-1-3">
-            <div class="uk-text-center"><div class="logo"><a href="/"><img src="/style/img/logo_bagyt.png"></a></div></div>
+            <div class="uk-text-center"><div class="logo"><a href=""><img src="/style/img/logo_bagyt.png"></a></div></div>
         </div>
         <div class="uk-width-1-3">
             <div class="uk-float-right dnpbtn">
@@ -64,13 +64,13 @@ ChangeLanguageAsset::register($this);
     <nav class="uk-navbar">
         <div class="uk-navbar-nav">
             <a href="#" class="multilanguage-set <?=Yii::$app->language=='kg-KG'?'lang lang-kg':'lang'; ?>" data-language="1">KG</a>
-            <a href="#" class="multilanguage-set <?=Yii::$app->language=='rus-RUS'?'lang lang-kg':'lang'; ?>" data-language="4">RU</a>
+            <a href="#" class="multilanguage-set <?=Yii::$app->language=='ru-RU'?'lang lang-kg':'lang'; ?>" data-language="2">RU</a>
             <a href="#" class="multilanguage-set <?=Yii::$app->language=='en-EN'?'lang lang-kg':'lang'; ?>" data-language="3">EN</a>
         </div>
 
         <div class="uk-navbar-flip">
-            <a href="<?=Yii::$app->params['facebook']?>" target="_blank" class="uk-icon-button uk-icon-facebook-square"></a>
-            <a href="<?=Yii::$app->params['instagram']?>" target="_blank" class="uk-icon-button uk-icon-instagram"></a>
+            <a href="<?=Yii::$app->params['facebook']?>" class="uk-icon-button uk-icon-facebook-square"></a>
+            <a href="<?=Yii::$app->params['instagram']?>" class="uk-icon-button uk-icon-instagram"></a>
         </div>
 
         <div class="uk-width uk-container-center uk-text-center uk-hidden-small" style="max-width: 575px">
@@ -80,21 +80,21 @@ ChangeLanguageAsset::register($this);
                 <?php }else{ ?>
                 <li><a href="<?=Url::toRoute('site/index')?>"><span class="uk-icon-home"></span></a></li>
                 <?php } if (Yii::t('app','sponsors')==$this->title){?>
-                <li class="uk-active"><a href="<?=Url::toRoute('site/sponsors')?>"><?=Yii::t('app','page_sponsors')?></a></li>
+                <li class="uk-active"><a href="<?=Url::toRoute('site/sponsors')?>">Демөөрчүлөр</a></li>
                 <?php }else{ ?>
-                <li><a href="<?=Url::toRoute('site/sponsors')?>"><?=Yii::t('app','page_sponsors')?></a></li>
+                <li><a href="<?=Url::toRoute('site/sponsors')?>">Демөөрчүлөр</a></li>
                 <?php } if (Yii::t('app','news')==$this->title){?>
-                <li class="uk-active"><a href="<?=Url::toRoute('site/news')?>"><?=Yii::t('app','page_news')?></a></li>
+                <li class="uk-active"><a href="<?=Url::toRoute('site/news')?>">Жаңылыктар</a></li>
                 <?php }else{ ?>
-                <li><a href="<?=Url::toRoute('site/news')?>"><?=Yii::t('app','page_news')?></a></li>
+                <li><a href="<?=Url::toRoute('site/news')?>">Жаңылыктар</a></li>
                 <?php } if (Yii::t('app','about')==$this->title){?>
-                <li class="uk-active"><a href="<?=Url::toRoute('site/about')?>"><?=Yii::t('app','page_about')?></a></li>
+                <li class="uk-active"><a href="<?=Url::toRoute('site/about')?>">Фонд тууралуу</a></li>
                 <?php }else{ ?>
-                <li><a href="<?=Url::toRoute('site/about')?>"><?=Yii::t('app','page_about')?></a></li>
+                <li><a href="<?=Url::toRoute('site/about')?>">Фонд тууралуу</a></li>
                 <?php } if (Yii::t('app','great_names')==$this->title){?>
-                <li class="uk-active"><a href="<?=Url::toRoute('site/great_names')?>"><?=Yii::t('app','page_people')?></a></li>
+                <li class="uk-active"><a href="<?=Url::toRoute('site/great_names')?>">Белгилүү инсандар</a></li>
                 <?php }else{ ?>
-                <li><a href="<?=Url::toRoute('site/great_names')?>"><?=Yii::t('app','page_people')?></a></li>
+                <li><a href="<?=Url::toRoute('site/great_names')?>">Белгилүү инсандар</a></li>
                 <?php } ?>
             </ul>
         </div>
@@ -110,11 +110,11 @@ ChangeLanguageAsset::register($this);
         <a href="<?=Url::toRoute('site/index')?>"><?=Yii::t('app','page_index')?></a> | <a href="<?=Url::toRoute('site/sponsors')?>"><?=Yii::t('app','page_sponsors')?></a> | <a href="<?=Url::toRoute('site/news')?>"><?=Yii::t('app','page_news')?></a> | <a href="<?=Url::toRoute('site/about')?>"><?=Yii::t('app','page_about')?></a> | <a href="<?=Url::toRoute('site/great_names');?>"><?=Yii::t('app','page_people')?></a>
         <hr>
         <div style="height: 40px">
-            <div class="call uk-margin-remove uk-float-left"><a href="tel:<?=' '.Yii::t('app','source_number')?>" style="color: #fff;"><span class="uk-icon-phone-square"></span> <?=' '.Yii::t('app','source_number')?></a></div>
+            <div class="call uk-margin-remove uk-float-left"><span class="uk-icon-phone-square"><?=' '.Yii::t('app','source_number')?></div>
             <div class="arrow-bottom-animate"><a href="#top" data-uk-smooth-scroll="" class="gototop"><img src="/style/img/gotop.png"></a></div>
             <div class="uk-float-right">
-                <a href="<?=Yii::$app->params['facebook']?>" target="_blank" class="uk-icon-button uk-icon-facebook-square"></a>
-                <a href="<?=Yii::$app->params['instagram']?>" target="_blank" class="uk-icon-button uk-icon-instagram"></a>
+                <a href="<?=Yii::$app->params['facebook']?>" class="uk-icon-button uk-icon-facebook-square"></a>
+                <a href="<?=Yii::$app->params['instagram']?>" class="uk-icon-button uk-icon-instagram"></a>
             </div>
         </div>
     </div>
@@ -124,33 +124,12 @@ ChangeLanguageAsset::register($this);
 
 <script src="/style/js/javas.js"></script>
 <script>
-function print_r(arr, level) {
-    var print_red_text = "";
-    if(!level) level = 0;
-    var level_padding = "";
-    for(var j=0; j<level+1; j++) level_padding += "    ";
-    if(typeof(arr) == 'object') {
-        for(var item in arr) {
-            var value = arr[item];
-            if(typeof(value) == 'object') {
-                print_red_text += level_padding + "'" + item + "' :\n";
-                print_red_text += print_r(value,level+1);
-		} 
-            else 
-                print_red_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
-        }
-    } 
-
-    else  print_red_text = "===>"+arr+"<===("+typeof(arr)+")";
-    return print_red_text;
-}
-   
- function Help_to() {
+    function Help_to() {
         var nameAnket = $('#aName').val();
         var phoneAnket = $('#aPhone').val();
         var emailAnket = $('#aEmail').val();
         $.ajax({
-            url: "<?=Url::toRoute('/site/helping');?>",
+            url: '/site/helping',
             type: "GET",
             data: {'name':nameAnket,'phone':phoneAnket,'email':emailAnket},
             success: function(data){
@@ -158,8 +137,8 @@ function print_r(arr, level) {
                 alert(data);
             },
             error: function (data) {
-                console.log(data);
-                console.log(print_r(data));
+//                console.log(data);
+                alert('Filed! '+data);
             }
         });
     }
@@ -168,7 +147,7 @@ function print_r(arr, level) {
         var phoneAnket = $('#aPhone2').val();
         var emailAnket = $('#aEmail2').val();
         $.ajax({
-            url: "<?=Url::toRoute('/site/helping');?>",
+            url: '/site/helping',
             type: "GET",
             data: {'name':nameAnket,'phone':phoneAnket,'email':emailAnket},
             success: function(data){
@@ -176,19 +155,12 @@ function print_r(arr, level) {
                 alert(data);
             },
             error: function (data) {
-                console.log(data);
-                alert('Filed! '+print_r(data[statusText]));
+//                console.log(data);
+                alert('Filed! '+data);
             }
         });
     }
 </script>
-
-<!-- BEGIN JIVOSITE CODE {literal} -->
-<script type='text/javascript'>
-(function(){ var widget_id = 'PflJNRA6wE';var d=document;var w=window;function l(){
-var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
-<!-- {/literal} END JIVOSITE CODE -->
-
 <?php $this->endBody() ?>
 </body>
 </html>
