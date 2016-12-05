c8caed1551c150ac55070b0fd9dc43b21bd76f5e
Nurik1kg <nurik1kg@gmail.com>
2016-12-06 01:29:47 +0600

    smtp setting

--- common/config/main.php
+++ common/config/main.php
@@ -33,8 +33,8 @@ return [
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
                 'host' => 'smtp.gmail.com',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
-                'username' => 'smtp.invest@gmail.com',
-                'password' => 'std12345ok',
+                'username' => 'kuttubagyt@gmail.com',
+                'password' => 'kairymduuluk',
                 'port' => '587', // Port 25 is a very common port too
                 'encryption' => 'tls', // It is often used, check your provider or mail server specs
             ],
@@ -45,8 +45,8 @@ return [
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
                 'host' => 'smtp.gmail.com',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
-                'username' => 'smtp.invest@gmail.com',
-                'password' => 'std12345ok',
+                'username' => 'kuttubagyt@gmail.com',
+                'password' => 'kairymduuluk',
                 'port' => '587', // Port 25 is a very common port too
                 'encryption' => 'tls', // It is often used, check your provider or mail server specs
             ],
