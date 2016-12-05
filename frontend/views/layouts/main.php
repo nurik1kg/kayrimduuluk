e15c45b330f4de6081101780b4dc6bf32e79441a
nurik1kg <nurik1kg@gmail.com>
2016-12-05 23:31:01 +0300

    kutbagyt.org

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -34,10 +34,10 @@ ChangeLanguageAsset::register($this);
 <div class="top">
     <div class="uk-grid uk-grid-collapse">
         <div class="uk-width-1-3">
-            <div class="call"><span class="uk-icon-phone-square"><?=' '.Yii::t('app','source_number')?></div>
+            <div class="call"><a href="tel:<?=' '.Yii::t('app','source_number')?>" style="color: #fff;"><span class="uk-icon-phone-square"></span> <?=' '.Yii::t('app','source_number')?></a></div>
         </div>
         <div class="uk-width-1-3">
-            <div class="uk-text-center"><div class="logo"><a href=""><img src="/style/img/logo_bagyt.png"></a></div></div>
+            <div class="uk-text-center"><div class="logo"><a href="/"><img src="/style/img/logo_bagyt.png"></a></div></div>
         </div>
         <div class="uk-width-1-3">
             <div class="uk-float-right dnpbtn">
@@ -69,8 +69,8 @@ ChangeLanguageAsset::register($this);
         </div>
 
         <div class="uk-navbar-flip">
-            <a href="<?=Yii::$app->params['facebook']?>" class="uk-icon-button uk-icon-facebook-square"></a>
-            <a href="<?=Yii::$app->params['instagram']?>" class="uk-icon-button uk-icon-instagram"></a>
+            <a href="<?=Yii::$app->params['facebook']?>" target="_blank" class="uk-icon-button uk-icon-facebook-square"></a>
+            <a href="<?=Yii::$app->params['instagram']?>" target="_blank" class="uk-icon-button uk-icon-instagram"></a>
         </div>
 
         <div class="uk-width uk-container-center uk-text-center uk-hidden-small" style="max-width: 575px">
@@ -110,11 +110,11 @@ ChangeLanguageAsset::register($this);
         <a href="<?=Url::toRoute('site/index')?>"><?=Yii::t('app','page_index')?></a> | <a href="<?=Url::toRoute('site/sponsors')?>"><?=Yii::t('app','page_sponsors')?></a> | <a href="<?=Url::toRoute('site/news')?>"><?=Yii::t('app','page_news')?></a> | <a href="<?=Url::toRoute('site/about')?>"><?=Yii::t('app','page_about')?></a> | <a href="<?=Url::toRoute('site/great_names');?>"><?=Yii::t('app','page_people')?></a>
         <hr>
         <div style="height: 40px">
-            <div class="call uk-margin-remove uk-float-left"><span class="uk-icon-phone-square"><?=' '.Yii::t('app','source_number')?></div>
+            <div class="call uk-margin-remove uk-float-left"><a href="tel:<?=' '.Yii::t('app','source_number')?>" style="color: #fff;"><span class="uk-icon-phone-square"></span> <?=' '.Yii::t('app','source_number')?></a></div>
             <div class="arrow-bottom-animate"><a href="#top" data-uk-smooth-scroll="" class="gototop"><img src="/style/img/gotop.png"></a></div>
             <div class="uk-float-right">
-                <a href="<?=Yii::$app->params['facebook']?>" class="uk-icon-button uk-icon-facebook-square"></a>
-                <a href="<?=Yii::$app->params['instagram']?>" class="uk-icon-button uk-icon-instagram"></a>
+                <a href="<?=Yii::$app->params['facebook']?>" target="_blank" class="uk-icon-button uk-icon-facebook-square"></a>
+                <a href="<?=Yii::$app->params['instagram']?>" target="_blank" class="uk-icon-button uk-icon-instagram"></a>
             </div>
         </div>
     </div>
@@ -124,12 +124,33 @@ ChangeLanguageAsset::register($this);
 
 <script src="/style/js/javas.js"></script>
 <script>
-    function Help_to() {
+function print_r(arr, level) {
+    var print_red_text = "";
+    if(!level) level = 0;
+    var level_padding = "";
+    for(var j=0; j<level+1; j++) level_padding += "    ";
+    if(typeof(arr) == 'object') {
+        for(var item in arr) {
+            var value = arr[item];
+            if(typeof(value) == 'object') {
+                print_red_text += level_padding + "'" + item + "' :\n";
+                print_red_text += print_r(value,level+1);
+        } 
+            else 
+                print_red_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
+        }
+    } 
+
+    else  print_red_text = "===>"+arr+"<===("+typeof(arr)+")";
+    return print_red_text;
+}
+   
+ function Help_to() {
         var nameAnket = $('#aName').val();
         var phoneAnket = $('#aPhone').val();
         var emailAnket = $('#aEmail').val();
         $.ajax({
-            url: '/site/helping',
+            url: "<?=Url::toRoute('/site/helping');?>",
             type: "GET",
             data: {'name':nameAnket,'phone':phoneAnket,'email':emailAnket},
             success: function(data){
@@ -137,8 +158,8 @@ ChangeLanguageAsset::register($this);
                 alert(data);
             },
             error: function (data) {
-//                console.log(data);
-                alert('Filed! '+data);
+                console.log(data);
+                console.log(print_r(data));
             }
         });
     }
@@ -147,7 +168,7 @@ ChangeLanguageAsset::register($this);
         var phoneAnket = $('#aPhone2').val();
         var emailAnket = $('#aEmail2').val();
         $.ajax({
-            url: '/site/helping',
+            url: "<?=Url::toRoute('/site/helping');?>",
             type: "GET",
             data: {'name':nameAnket,'phone':phoneAnket,'email':emailAnket},
             success: function(data){
@@ -155,12 +176,19 @@ ChangeLanguageAsset::register($this);
                 alert(data);
             },
             error: function (data) {
-//                console.log(data);
-                alert('Filed! '+data);
+                console.log(data);
+                alert('Filed! '+print_r(data[statusText]));
             }
         });
     }
 </script>
+
+<!-- BEGIN JIVOSITE CODE {literal} -->
+<script type='text/javascript'>
+(function(){ var widget_id = 'PflJNRA6wE';var d=document;var w=window;function l(){
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
+<!-- {/literal} END JIVOSITE CODE -->
+
 <?php $this->endBody() ?>
 </body>
 </html>
