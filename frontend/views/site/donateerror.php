8da4658208b2a0921f503756c22e08569f32e7a4
Nurik1kg <nurik1kg@gmail.com>
2016-12-19 12:03:39 +0600

    donate edit2

--- frontend/views/site/donateerror.php
+++ frontend/views/site/donateerror.php
@@ -22,7 +22,7 @@ switch (Yii::$app->language) {
     <div class="uk-modal-dialog uk-border-rounded" style="max-width: 350px; top: 200px;">
         <form id="w1" class="uk-form" action="/site/feedback" method="post">
             <input type="hidden" name="_csrf-frontend" value="Z3Rodk5LMmxWFz8aPBJqOwYDJBIDGX02Fh8BHSIGYDwtOlocJSlnGQ==">                            <fieldset data-uk-margin="">
-                <legend><i class="fa fa-check ff" aria-hidden="true"><?=$message?></i> </legend>
+                <legend><i class="fa fa-close ff" aria-hidden="true"><?=$message?></i> </legend>
             </fieldset>
         </form>
     </div>
